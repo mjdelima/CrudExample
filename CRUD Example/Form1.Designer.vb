08cdb118c695a06c65f7152096cf30cbe0a9312d
Mc Joshua de Lima <mcjoshua1@yahoo.com>
2021-11-29 17:39:00 +0800

    1st version
    The program presented during the class discussion

---
+++ CRUD Example/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.tbFirst = New System.Windows.Forms.TextBox()
+        Me.tbMiddle = New System.Windows.Forms.TextBox()
+        Me.tbLastName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.tbSearch = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tbFirst
+        '
+        Me.tbFirst.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbFirst.Location = New System.Drawing.Point(135, 41)
+        Me.tbFirst.Margin = New System.Windows.Forms.Padding(4)
+        Me.tbFirst.Name = "tbFirst"
+        Me.tbFirst.Size = New System.Drawing.Size(804, 34)
+        Me.tbFirst.TabIndex = 0
+        '
+        'tbMiddle
+        '
+        Me.tbMiddle.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbMiddle.Location = New System.Drawing.Point(135, 122)
+        Me.tbMiddle.Margin = New System.Windows.Forms.Padding(4)
+        Me.tbMiddle.Name = "tbMiddle"
+        Me.tbMiddle.Size = New System.Drawing.Size(804, 34)
+        Me.tbMiddle.TabIndex = 1
+        '
+        'tbLastName
+        '
+        Me.tbLastName.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbLastName.Location = New System.Drawing.Point(135, 203)
+        Me.tbLastName.Margin = New System.Windows.Forms.Padding(4)
+        Me.tbLastName.Name = "tbLastName"
+        Me.tbLastName.Size = New System.Drawing.Size(804, 34)
+        Me.tbLastName.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 44)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 28)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "First Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 131)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(119, 28)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Midle Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(21, 212)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(103, 28)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Last Name"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
+        Me.DataGridView1.Location = New System.Drawing.Point(21, 308)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 29
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(918, 301)
+        Me.DataGridView1.TabIndex = 6
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(135, 256)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(125, 36)
+        Me.btnAdd.TabIndex = 7
+        Me.btnAdd.Text = "ADD"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(266, 256)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(125, 36)
+        Me.btnUpdate.TabIndex = 8
+        Me.btnUpdate.Text = "UPDATE"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(402, 256)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(125, 36)
+        Me.btnDelete.TabIndex = 9
+        Me.btnDelete.Text = "DELETE"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'tbSearch
+        '
+        Me.tbSearch.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbSearch.Location = New System.Drawing.Point(542, 257)
+        Me.tbSearch.Name = "tbSearch"
+        Me.tbSearch.Size = New System.Drawing.Size(397, 34)
+        Me.tbSearch.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 28.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(968, 621)
+        Me.Controls.Add(Me.tbSearch)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tbLastName)
+        Me.Controls.Add(Me.tbMiddle)
+        Me.Controls.Add(Me.tbFirst)
+        Me.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbFirst As TextBox
+    Friend WithEvents tbMiddle As TextBox
+    Friend WithEvents tbLastName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents tbSearch As TextBox
+End Class
---
+++ CRUD Example/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Data
+Public Class Form1
+    Dim con As MySqlConnection
+    Dim cmd As New MySqlCommand
+    Dim dataadapter As New MySqlDataAdapter
+    Dim datatable As New DataTable
+
+    Sub data_load(ByVal data As String)
+        con = New MySqlConnection()
+        con.ConnectionString = "server=127.0.0.1; userid=root; password=12345; database=crud"
+
+        Try
+
+            datatable.Clear()
+
+            If con.State = ConnectionState.Closed Then
+                con.Open()
+            End If
+
+            cmd.CommandText = data
+            cmd.Connection = con
+            dataadapter.SelectCommand = cmd
+            dataadapter.Fill(datatable)
+
+            With DataGridView1
+                .DataSource = datatable
+                .Columns(0).HeaderText = "ID"
+                .Columns(1).HeaderText = "First Name"
+                .Columns(2).HeaderText = "Middle Name"
+                .Columns(3).HeaderText = "Last Name"
+                .Columns(0).Width = 50
+                .Columns(1).Width = 150
+                .Columns(2).Width = 150
+                .Columns(3).Width = 150
+            End With
+
+            con.Close()
+
+        Catch ex As Exception
+            MessageBox.Show("Error" & ex.Message.ToString)
+        Finally
+            con.Dispose()
+        End Try
+
+
+
+
+
+    End Sub
+
+    Private Sub Query(ByVal data As String)
+        Try
+            If con.State = ConnectionState.Closed Then
+                con.Open()
+            End If
+
+
+            cmd.Connection = con
+            cmd.CommandText = data
+            cmd.ExecuteNonQuery()
+
+            tbFirst.Clear()
+            tbLastName.Clear()
+            tbMiddle.Clear()
+
+            data_load("SELECT id, fname, mname, lname FROM users")
+            con.Close()
+
+
+        Catch ex As Exception
+            MessageBox.Show("Error" & ex.Message.ToString)
+        Finally
+            con.Dispose()
+
+        End Try
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        data_load("SELECT id, fname, mname, lname FROM users")
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Query("INSERT INTO users(fname, mname, lname) VALUES('" & tbFirst.Text _
+              & "','" & tbMiddle.Text & "','" & tbLastName.Text & "')")
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        tbFirst.Text = DataGridView1.CurrentRow.Cells(1).Value.ToString
+        tbMiddle.Text = DataGridView1.CurrentRow.Cells(2).Value.ToString
+        tbLastName.Text = DataGridView1.CurrentRow.Cells(3).Value.ToString
+
+
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Query("UPDATE users SET fname='" & tbFirst.Text & "', mname='" & tbMiddle.Text & "', lname='" _
+              & tbLastName.Text & "' WHERE id=" & DataGridView1.CurrentRow.Cells(0).Value.ToString)
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Query("DELETE FROM users WHERE id=" & DataGridView1.CurrentRow.Cells(0).Value.ToString)
+    End Sub
+
+    Private Sub tbSearch_TextChanged(sender As Object, e As EventArgs) Handles tbSearch.TextChanged
+        data_load("SELECT id, fname, mname, lname FROM users" _
+                  & "WHERE fname = '" & tbSearch.Text & "' OR lname='" & tbSearch.Text & "'")
+
+    End Sub
+End Class
